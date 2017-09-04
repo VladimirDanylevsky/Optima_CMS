584c2d40b744c55357ae1c8b8d5138c75bb98e5e
Valder <danylevsky.valder@gmail.com>
2017-09-04 05:21:55 +0300

    Some changes

--- optima/optima/settings/base.py
+++ optima/optima/settings/base.py
@@ -28,8 +28,8 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     'home',
     'search',
-    'admin_customization', # Customization for admin page, use only for it, do not add new models/views.
-
+    'admin_customization',  # Customization for admin page, use only for it, do not add new models/views.
+    'catalog',              # Main app
     'wagtail.wagtailforms',
     'wagtail.wagtailredirects',
     'wagtail.wagtailembeds',
@@ -141,3 +141,6 @@ WAGTAIL_SITE_NAME = "optima"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+# Make high-quality images
+WAGTAILIMAGES_JPEG_QUALITY = 90
